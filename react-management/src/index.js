4f073fb7616e34380127a1145c71d3cbaa688ee8
徐文斌 <xuwenbin@bmsoft.com.cn>
2020-04-08 09:05:07 +0800

    路由引入 antd

--- react-management/src/index.js
+++ react-management/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './page/app'
+import 'antd/dist/antd.css'
+import AppRouter from './router/index'
 
-ReactDOM.render(<App />,document.getElementById('root') );
+ReactDOM.render(<AppRouter />, document.getElementById('root'));
 
 
---
+++ react-management/src/router/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Button } from 'antd';
+
+class AppRouter extends Component {
+    render() {
+        return (
+            <Router>
+                <div className="main">
+                    <Switch>
+                   
+                    </Switch>
+                </div>
+            </Router>
+        );
+    }
+}
+
+export default AppRouter;
