59f3973803f6f4eda8fd3e9c2eac036e4fe1552f
徐文斌 <xuwenbin@bmsoft.com.cn>
2020-08-17 11:16:07 +0800

    线上地址替换

--- react-management/src/services/axios.js
+++ react-management/src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'http://127.0.0.1:7001/';
-// axios.defaults.baseURL = 'http://47.93.16.138:7001/';
+// axios.defaults.baseURL = 'http://127.0.0.1:7001/';
+axios.defaults.baseURL = 'http://47.93.16.138:7001/';
 axios.defaults.headers = { "X-Requested-With": "XMLHttpRequest" };
 // axios.defaults.timeout = 5000;
 // 请求拦截
