c0db123f33a7f556431e1f1a188d77f08e6b0234
徐文斌 <xuwenbin@bmsoft.com.cn>
2020-08-17 10:28:05 +0800

    新增调整

--- good-mom/src/page/System/Project/add.js
+++ good-mom/src/page/System/Project/add.js
@@ -1,18 +1,105 @@
 import React, { Component } from "react";
-import { withRouter } from "react-router-dom";
 
 import "./style/add.scss";
+
+import { Form, Input, Button, Upload, message } from "antd";
+import { LoadingOutlined, PlusOutlined } from "@ant-design/icons";
+
+const layout = {
+  labelCol: { span: 4 },
+  wrapperCol: { span: 20 },
+};
+const tailLayout = {
+  wrapperCol: { offset: 8, span: 16 },
+};
+
+function beforeUpload(file) {
+  const isJpgOrPng = file.type === "image/jpeg" || file.type === "image/png";
+  if (!isJpgOrPng) {
+    message.error("You can only upload JPG/PNG file!");
+  }
+  const isLt2M = file.size / 1024 / 1024 < 2;
+  if (!isLt2M) {
+    message.error("Image must smaller than 2MB!");
+  }
+  return isJpgOrPng && isLt2M;
+}
 
 export class add extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      loading: false,
+    };
   }
 
+  onFinish = (values) => {
+    console.log("Success:", values);
+  };
+
+  onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
   render() {
-    return <div className="system-project-add">新增项目</div>;
+    const uploadButton = (
+      <div>
+        {this.state.loading ? <LoadingOutlined /> : <PlusOutlined />}
+        <div className="ant-upload-text">Upload</div>
+      </div>
+    );
+    const { imageUrl } = this.state;
+    return (
+      <div className="system-project-add">
+        <div className="form-body">
+          <Form
+            {...layout}
+            name="basic"
+            initialValues={{ remember: true }}
+            onFinish={this.onFinish}
+            onFinishFailed={this.onFinishFailed}
+          >
+            <Form.Item
+              label="项目名称："
+              name="name"
+              rules={[{ required: true, message: "请输入项目名称！" }]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item label="项目简介：" name="describe">
+              <Input />
+            </Form.Item>
+
+            <Form.Item label="图片：" name="img">
+              <Upload
+                name="avatar"
+                listType="picture-card"
+                className="avatar-uploader"
+                showUploadList={false}
+                action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                beforeUpload={beforeUpload}
+                onChange={this.handleChange}
+              >
+                {imageUrl ? (
+                  <img src={imageUrl} alt="avatar" style={{ width: "100%" }} />
+                ) : (
+                  uploadButton
+                )}
+              </Upload>
+            </Form.Item>
+
+            <Form.Item {...tailLayout}>
+              <Button htmlType="button">取消</Button>
+              <Button type="primary" htmlType="submit">
+                提交
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
+      </div>
+    );
   }
 }
 
-// export default add;
-export default withRouter(add);
+export default add;
